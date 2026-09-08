e1636e97d2e3530a6f3ea473c58ba94bb2902dc2
tirthankar95 <tirthankarmittra@gmail.com>
2026-09-08 16:43:56 +0530

    Change network

--- examples/card_arithmetic/network.py
+++ examples/card_arithmetic/network.py
@@ -1,33 +1,42 @@
-from torch import nn
+import torch
 
-
-class Net(nn.Module):
+class CardNet(torch.nn.Module):
     def __init__(self):
+        """
+        Initialize a neural network for processing playing card images
+        """
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3)
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.conv3 = nn.Conv2d(32, 32, 3)
-        self.conv4 = nn.Conv2d(32, 64, 3)
-        self.classifier = nn.Conv2d(64, 52, 1)
 
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.flatten = nn.Flatten()
-        self.dropout = nn.Dropout(0.5)
-        self.ReLU = nn.ReLU()
-        self.softmax = nn.Softmax(1)
+        self.conv1 = torch.nn.Conv2d(3, 64, 3)
+        self.conv2 = torch.nn.Conv2d(64, 64, 3)
+        self.conv3 = torch.nn.Conv2d(64, 128, 3)
+        self.conv4 = torch.nn.Conv2d(128, 128, 3)
+        self.fc1 = torch.nn.Linear(17280, 512)
+        self.fc2 = torch.nn.Linear(512, 52)
 
-    def forward(self, x, marg_idx=None, type=1):
-        if x.dim() == 5:
-            x = x.flatten(0, 1)
-        x = self.ReLU(self.conv1(x))
-        x = self.ReLU(self.conv2(x))
-        x = self.ReLU(self.conv3(x))
-        x = self.ReLU(self.conv4(x))
-        x = self.dropout(x)
-        x = self.classifier(x)
-        x = self.pool(x)
+        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.flatten = torch.nn.Flatten()
+        self.dropout = torch.nn.Dropout(0.5)
+        self.ReLU = torch.nn.ReLU()
+        self.softmax = torch.nn.Softmax(1)
+
+
+    def forward(self, x):
+        """
+        Process the input image
+        :param x: Batched input images
+        :return: Prediction of downstream label as a logit tensor
+        """
+        x = x.flatten(0, 1)
+        x = self.pool(self.ReLU(self.conv1(x)))
+        x = self.pool(self.ReLU(self.conv2(x)))
+        x = self.pool(self.ReLU(self.conv3(x)))
+        x = self.pool(self.ReLU(self.conv4(x)))
 
         x = self.flatten(x)
+        x = self.dropout(x)
+
+        x = self.fc1(x)
+        x = self.fc2(x)
         x = self.softmax(x)
         return x
-
--- tests/run.sh
+++ tests/run.sh
@@ -4,4 +4,4 @@ export PYTHONPATH="$(pwd)/.."
 # python3 -m unittest -v test_speeds.TestSpeeds.test_speeds_mnist_add
 # python3 -m unittest -v test_speeds.TestSpeeds.test_speeds_add2x2
 python3 -m unittest -v test_speeds.TestSpeeds.test_speeds_card_arithmetic_2sum
-#python3 -m unittest -v test_speeds.TestSpeeds.test_speeds_card_arithmetic_3sum
+# python3 -m unittest -v test_speeds.TestSpeeds.test_speeds_card_arithmetic_3sum
